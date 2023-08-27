28859affc1cf39f6b65ba291307668f90d03abf9
Cvrishi07 <115592207+Cvrishi07@users.noreply.github.com>
2023-08-27 00:23:02 +0000

    dev configuration set

---
+++ Basics/Variables.js
@@ -0,0 +1,9 @@
+const accountId = 14 
+let accountEmail = "cvrishi@gmail.com" 
+var accountPassword = "12345" 
+
+accountCity = "varanasi" 
+
+console.log(accountCity) ;
+
+console.table([accountId,accountPassword,accountEmail]) ; //Display multiple items at once 
