1734da2fce05f49b7ae6fe8f6cc83be7afaa5c8d
Cvrishi07 <115592207+Cvrishi07@users.noreply.github.com>
2023-08-28 20:14:00 +0000

    Conversion of datatypes

---
+++ Basics/Conversion.js
@@ -0,0 +1,30 @@
+let score = 33 
+let score1 = true 
+let score2 = "33abc"
+let score3 = undefined 
+let score4 = null 
+let score5 = "Chaitanya"
+
+let valueOfNumber = Number(score);
+let valueOfNumber1 = Number(score1);
+let valueOfNumber2 = Number(score2) ;
+let valueOfNumber3 = Number(score3) ;
+let valueOfNumber4 = Number(score4);
+let valueOfNumber5 = Number(score5) ;
+
+console.log(typeof valueOfNumber)
+console.log(typeof valueOfNumber1)
+console.log(typeof valueOfNumber2)
+console.log(typeof valueOfNumber3)
+console.log(typeof valueOfNumber4)
+console.log(typeof valueOfNumber5)
+
+console.log(valueOfNumber)
+console.log(valueOfNumber1)
+console.log(valueOfNumber2)
+console.log(valueOfNumber3)
+console.log(valueOfNumber4)
+console.log(valueOfNumber5)
+
+// 1 => true , 0 => false 
+// "" => false , "Rishi" => true
