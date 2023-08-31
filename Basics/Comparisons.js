a8bbbc3cbfafe7933c4105ba8512a1bf5811f17b
Cvrishi07 <115592207+Cvrishi07@users.noreply.github.com>
2023-08-31 16:03:17 +0000

    Comparisons in js

---
+++ Basics/Comparisons.js
@@ -0,0 +1,22 @@
+// Comparisons to avoid in javascript 
+// Cmparisons between different datatypes should be avoided because it leads to  unpredictability in result
+
+console.log("2" > 1 ) ;
+console.log("02" >= 1) ;
+
+console.log(null > 0);
+console.log(null == 0) ;
+console.log(null >= 0) ;
+
+console.log(undefined > 0);
+console.log(undefined == 0) ;
+console.log(undefined >= 0) ;
+
+
+// Normal Check vs Strict check 
+
+console.log("1" == 1) 
+console.log("1" === 1) 
+
+// Strict check compares number as well as datatype. 
+
