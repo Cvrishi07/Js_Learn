545cc330c24b5fa305ba17e944016ca8f4e14db0
Cvrishi07 <115592207+Cvrishi07@users.noreply.github.com>
2023-09-13 18:31:30 +0000

    Stacks and heap

--- Basics/Datatypes-summary.js
+++ Basics/Datatypes-summary.js
@@ -33,7 +33,7 @@ const myFunction = function(){
     console.log("Hello World");
 }
 
-console.log(typeof bigNumber) ;       // Typeof BifInt is BigInt
+console.log(typeof bigNumber) ;       // Typeof BigInt is BigInt
 console.log(typeof outsideTemp) ;    // Typeof null is object
 console.log(typeof Id) ;             // Typeof Symbol is Symbol 
 console.log(typeof isLoggedIn) ;    // Typeof boolean is boolean
@@ -42,3 +42,35 @@ console.log(typeof scorevalue) ;    //Typeof number is number
 console.log(typeof heroes);         // Typeof array is object
 console.log(typeof myObj) ;         //Typeof object is object 
 console.log(typeof myFunction);     //Typeof function is function
+
+// Stack and Heap memory 
+// Stack (Primitive) 
+// Heap (Non-primitive) 
+
+let myName = "Rishi" ;
+let anotherName = myName ; 
+
+console.log(myName);
+console.log(anotherName) ;
+ 
+anotherName = "cvrishi" ;
+
+console.log(myName) ;
+console.log(anotherName);
+
+let newObj = {
+    email: "scaryhitman@gmail.com",
+    upi: "ybl@gmail.com"
+}
+let newObj2 = newObj ;
+
+console.log(newObj) ;
+console.log(newObj2) ;
+
+newObj2.email = "realcv07@gmail.com" ; 
+
+console.log(newObj);
+console.log(newObj2);
+
+
+
